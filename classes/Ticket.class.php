b72eb5aac85e0b96413b1be7698c77f3cdf1cb7e
vanessa1408 <vanvisse@gmail.com>
2022-04-10 12:16:29 +0200

    Version_10-04-22

---
+++ classes/Ticket.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class Ticket {
+    private $IdTicket;
+    private $DateAppelClient;
+    private $DatePEC;
+    private $DateFermTicket;
+    private $IdType;
+    private $IDTypeInter;
+    private $IdCommande;
+    private $IdTechnicien;
+    private $IdTechnicien_1;
+
+    public function __construct()
+
+}
+
+
+?>
